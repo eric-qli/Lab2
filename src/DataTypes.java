6e184e9153f53e4631abd76b2cc825353923d16c
eric-qli <92590396+eric-qli@users.noreply.github.com>
2023-09-18 17:47:14 -0400

    111

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,7 +1,6 @@
 import java.util.List;
 
 public class DataTypes {
-    // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
     public static long sum(List<Integer> numbers) {
 
         int s = 0;
@@ -9,6 +8,7 @@ public class DataTypes {
         for (int x : numbers) {
             s += x;
         }
-        return s;
+        long a = s;
+        return a;
     }
 }
